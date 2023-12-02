423e175e3a336af873f512b16cf2560ccdcaf971
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-12-02 12:46:46 +0900

    succeeded to get address

--- index.ts
+++ index.ts
@@ -1,17 +1,37 @@
-import ethers from 'ethers';
+import { ethers } from 'ethers';
+import { config } from 'dotenv';
+config();
+const API_KEY = process.env.API_KEY;
 
-const providerUrl = "https://eth-goerli.g.alchemy.com/v2/lQQcdlj4Fye1AKw5R94wVNA-BDKevm0W";
+const providerUrl = `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`;
 const provider = new ethers.JsonRpcProvider(providerUrl);
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const abi = [
+  "function feth() external view returns (address)"
+];
 
-async function getContractCode(address: string) {
+const contractAddress = "0xFd17FF21a39E5440b4b3652ca80f15b6FF938A51";
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+async function getFeth() {
   try {
-    const code = await provider.getCode(address);
-    console.log("Contract Code", code);
+    const address = await contract.feth()
+    console.log("FETH Address", address);
   } catch (error) {
     console.error("Error", error);
   }
 }
 
-getContractCode(contractAddress);
+getFeth();
+
+
+// async function getContractCode(address: string) {
+//   try {
+//     const code = await provider.getCode(address);
+//     console.log("Contract Code", code);
+//   } catch (error) {
+//     console.error("Error", error);
+//   }
+// }
+
+// getContractCode(contractAddress);
