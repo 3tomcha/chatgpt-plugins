13907fd2d6d152212ce414db71a8f96fd5d200db
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-12-02 15:52:05 +0900

    succeeded to get abi

--- index.ts
+++ index.ts
@@ -1,37 +1,41 @@
 import { ethers } from 'ethers';
 import { config } from 'dotenv';
+import axios from 'axios';
+
 config();
+
 const API_KEY = process.env.API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
-const providerUrl = `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`;
-const provider = new ethers.JsonRpcProvider(providerUrl);
-
-const abi = [
-  "function feth() external view returns (address)"
-];
-
-const contractAddress = "0xFd17FF21a39E5440b4b3652ca80f15b6FF938A51";
-const contract = new ethers.Contract(contractAddress, abi, provider);
-
-async function getFeth() {
+const getAbi = async () => {
   try {
-    const address = await contract.feth()
-    console.log("FETH Address", address);
+    const abi = await axios.get("https://api.etherscan.io/api?module=contract&action=getabi&address=0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359&apikey=" + ETHERSCAN_API_KEY);
+    console.log(abi);
+    return abi;
   } catch (error) {
     console.error("Error", error);
   }
 }
 
-getFeth();
+const abi = getAbi();
 
+// const providerUrl = `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`;
+// const provider = new ethers.JsonRpcProvider(providerUrl);
 
-// async function getContractCode(address: string) {
+// const abi = [
+//   "function feth() external view returns (address)"
+// ];
+
+// const contractAddress = "0xFd17FF21a39E5440b4b3652ca80f15b6FF938A51";
+// const contract = new ethers.Contract(contractAddress, abi, provider);
+
+// async function getFeth() {
 //   try {
-//     const code = await provider.getCode(address);
-//     console.log("Contract Code", code);
+//     const address = await contract.feth()
+//     console.log("FETH Address", address);
 //   } catch (error) {
 //     console.error("Error", error);
 //   }
 // }
 
-// getContractCode(contractAddress);
+// getFeth();
